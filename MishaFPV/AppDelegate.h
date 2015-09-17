189c4950ea300992f63490badabfa6b0d914d07b
Vitaly Berg <bergusman@gmail.com>
2015-09-17 20:34:04 +0300

    Initial Commit

---
+++ MishaFPV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MishaFPV
+//
+//  Created by Vitaly Berg on 17/09/15.
+//  Copyright © 2015 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MishaFPV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MishaFPV
+//
+//  Created by Vitaly Berg on 17/09/15.
+//  Copyright © 2015 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
